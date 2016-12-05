960a3f5ddf281f7dea519f2b50edaa13078b2b31
Sean Talts <sean.talts@gmail.com>
2016-12-05 17:15:52 -0500

    Initial Commit

---
+++ nav2Tests/nav2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  nav2Tests.swift
+//  nav2Tests
+//
+//  Created by Sean Talts on 12/5/16.
+//  Copyright © 2016 Sean Talts. All rights reserved.
+//
+
+import XCTest
+@testable import nav2
+
+class nav2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
