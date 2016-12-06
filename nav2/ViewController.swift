4eb3775965dc941a9654df227ed707520a81b7ca
Sean Talts <sean.talts@gmail.com>
2016-12-05 20:58:42 -0500

    Init

--- nav2/ViewController.swift
+++ nav2/ViewController.swift
@@ -7,17 +7,55 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    var lm : CLLocationManager!
+    var headingOverlay : UILabel!
+    var locationOverlay : UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.setupLocationStuff()
+        
+        headingOverlay = UILabel()
+        headingOverlay.frame = CGRect(x: 10, y: 300, width: 200, height: 200)
+        headingOverlay.text = "Start heading text"
+        self.view.addSubview(headingOverlay)
+
+        locationOverlay = UILabel()
+        locationOverlay.frame = CGRect(x: 10, y: 200, width: 600, height: 100)
+        locationOverlay.textColor = UIColor.red
+        locationOverlay.text = "Start location text"
+        self.view.addSubview(locationOverlay)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func setupLocationStuff() {
+        lm = CLLocationManager()
+        lm.delegate = self
+        lm.requestWhenInUseAuthorization()
+        lm.headingFilter = kCLHeadingFilterNone
+        lm.distanceFilter = kCLDistanceFilterNone
+        lm.startUpdatingLocation()
+        lm.startUpdatingHeading()
+        print("done setting up location stuff")
+    }
+
+    internal func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        headingOverlay.text = "\(newHeading.trueHeading)"
+    }
+
+    internal func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let coord = locations.last?.coordinate {
+            locationOverlay.text = "\(coord.latitude), \(coord.longitude)"
+        }
+        print("update \(locations) ")
     }
 
 
